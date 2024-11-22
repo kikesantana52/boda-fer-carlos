4abe804b80484c27f4758ce44d8e46b7c3eb7f74
Luis Santana <sistemas@ecomexico.mx>
2024-11-22 13:00:12 -0600

    Use respuestas

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,22 @@ import Confirmacion from "./components/Confirmacion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
+import * as XLSX from "xlsx";
+import { useSearchParams } from "react-router-dom";
+
 function App() {
   const [playStatus, setplayStatus] = useState<
     "STOPPED" | "PLAYING" | "PAUSED"
   >("STOPPED");
+  const [numeroBoletos, setNumeroBoletos] = useState(0);
+  const [familia, setFamilia] = useState("");
+  const [data, setdata] = useState<any>();
+  const [row, setRow] = useState(0);
+  const [searchParams] = useSearchParams();
+  const codigoInvitacion = searchParams.get("codigoInvitacion");
+
   useEffect(() => {
+    // Fade in effects
     const fadeInSections = document.querySelectorAll("section");
     const fadeInOnScroll = () => {
       fadeInSections.forEach((section) => {
@@ -36,56 +47,108 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    const fetchInvitaciones = async () => {
+      const s3Url =
+        "https://fyc08022025.s3.us-east-1.amazonaws.com/invitaciones.xlsx";
+      try {
+        const response = await fetch(s3Url);
+        const blob = await response.blob();
+        const file = new File([blob], "archivo.xlsx");
+
+        const reader = new FileReader();
+        reader.onload = (event) => {
+          const binaryStr = event?.target?.result;
+          const workbook = XLSX.read(binaryStr, { type: "binary" });
+          const sheetName = workbook.SheetNames[0];
+          const worksheet = workbook.Sheets[sheetName];
+          const jsonData: any[][] = XLSX.utils.sheet_to_json(worksheet, {
+            header: 1,
+          });
+          const row = jsonData.findIndex((row) => row[2] === codigoInvitacion);
+          const invitacion = jsonData.find(
+            (row) => row[2] === codigoInvitacion
+          );
+
+          if (invitacion) {
+            setNumeroBoletos(invitacion[1]);
+            setFamilia(invitacion[0]);
+            setRow(row);
+            setdata(jsonData);
+          } else {
+            console.log("Sin invitación");
+          }
+        };
+        reader.readAsBinaryString(file);
+      } catch (error) {
+        console.error("Error al leer el archivo desde S3:", error);
+      }
+    };
+
+    if (codigoInvitacion) {
+      fetchInvitaciones();
+    }
+  }, [codigoInvitacion]);
+
   return (
     <div className="App">
-      <Sound
-        url="https://cdn.bazehits.com/uploads/music/2024/05/Billie-Eilish-BIRDS-OF-AHER-(Bazehits).mp3"
-        playStatus={playStatus}
-      />
-      <button
-        className="music"
-        onClick={() =>
-          setplayStatus(playStatus !== "PLAYING" ? "PLAYING" : "PAUSED")
-        }
-      >
-        <FontAwesomeIcon icon={faMusic} />
-      </button>
-      <div className="desktop-only">
-        <img src={logo} alt=" " />
-        <p className="bellefair-regular">
-          Esta página solo es compatible con dispositivos móviles. Por favor,
-          ábrela en un dispositivo móvil.
-        </p>
-      </div>
-
-      <div className="mobile-only">
-        <section id="home">
-          <h1 className="cinzel-decorative-regular">Fernanda & Carlos</h1>
-          <div className="divider"></div>
-          <h2 className="lato-regular">08.FEB.2025</h2>
-        </section>
-
-        <Contador />
-
-        <CuandoDonde />
-
-        <CodigoVestimenta />
-
-        <Galeria />
-
-        <MesaRegalos />
-
-        <Confirmacion />
-
-        <div id="despedida">
-          <h2 className="bellefair-regular">
-            Es un honor contar con tu presencia en este dia tan especial para
-            nosotros.
-          </h2>
-
-          <p className="cinzel-decorative-regular">
-            Con mucho cariño: Fernanda & Carlos.
+      <div>
+        <Sound
+          url="https://cdn.bazehits.com/uploads/music/2024/05/Billie-Eilish-BIRDS-OF-AHER-(Bazehits).mp3"
+          playStatus={playStatus}
+        />
+        <button
+          className="music"
+          onClick={() =>
+            setplayStatus(playStatus !== "PLAYING" ? "PLAYING" : "PAUSED")
+          }
+        >
+          <FontAwesomeIcon icon={faMusic} />
+        </button>
+        <div className="desktop-only">
+          <img src={logo} alt=" " />
+          <p className="bellefair-regular">
+            Esta página solo es compatible con dispositivos móviles. Por favor,
+            ábrela en un dispositivo móvil.
           </p>
+        </div>
+
+        <div className="mobile-only">
+          <section id="home">
+            <h1 className="cinzel-decorative-regular">Fernanda & Carlos</h1>
+            <div className="divider"></div>
+            <h2 className="lato-regular">08.FEB.2025</h2>
+          </section>
+
+          <Contador />
+
+          <CuandoDonde />
+
+          <CodigoVestimenta />
+
+          <Galeria />
+
+          <MesaRegalos />
+
+          {numeroBoletos > 0 && (
+            <Confirmacion
+              numBoletos={numeroBoletos}
+              familia={familia}
+              initialData={data}
+              posicion={row}
+            />
+          )}
+
+          <div id="despedida">
+            <h2 className="bellefair-regular">
+              Es un honor contar con tu presencia en este dia tan especial para
+              nosotros.
+            </h2>
+
+            <p className="cinzel-decorative-regular">
+              Con mucho cariño: Fernanda & Carlos.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Confirmacion.tsx
+++ src/components/Confirmacion.tsx
@@ -1,55 +1,148 @@
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 
-function Confirmacion() {
-  const [numBoletos, setNumBoletos] = useState(3);
-  // URL: localhost?codigoInvitacion=AA1
-  const [searchParams] = useSearchParams();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const codigoInvitacion = searchParams.get("codigoInvitacion");
+import AWS from "aws-sdk";
+import * as XLSX from "xlsx";
+import { CircleLoader } from "react-spinners";
+
+AWS.config.update({
+  accessKeyId: "AKIAUYCRGIUBKLBKOB3Z",
+  secretAccessKey: "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",
+  region: "us-east-1", // Ejemplo:
+});
+
+const s3 = new AWS.S3();
+
+interface IProps {
+  numBoletos: number;
+  familia: string;
+  initialData: any;
+  posicion: number;
+}
+function Confirmacion({ numBoletos, familia, initialData, posicion }: IProps) {
+  const [subiendo, setSubiendo] = useState(false);
+  const [respuestaEnviada, setRespuestaEnviada] = useState(false);
+  const [boletosConfirmados, setBoletosConfirmados] =
+    useState<number>(numBoletos);
+  const [mensaje, setMensaje] = useState("");
+
+  useEffect(() => {
+    setBoletosConfirmados(numBoletos);
+  }, [numBoletos]);
+
+  const updateExcel = async (data: any) => {
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Hoja1");
+
+    // Crear el archivo Excel
+    const excelBuffer = XLSX.write(workbook, {
+      bookType: "xlsx",
+      type: "array",
+    });
+
+    // Subir a S3
+    const params = {
+      Bucket: "fyc08022025",
+      Key: "invitaciones.xlsx",
+      Body: new Blob([excelBuffer], { type: "application/octet-stream" }),
+      ContentType:
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    };
+
+    s3.upload(params, (err: any, data: any) => {
+      if (err) {
+        console.error("Error al subir el archivo:", err);
+      } else {
+        setSubiendo(false);
+        setRespuestaEnviada(true);
+        console.log("Archivo subido con éxito:", data.Location);
+      }
+    });
+  };
+
+  const handleSubmit = (type: "ASISTIRE" | "NO ASISTIRE") => {
+    initialData[posicion][3] = type === "ASISTIRE" ? boletosConfirmados : 0;
+    initialData[posicion][4] = mensaje;
+    setSubiendo(true);
+    updateExcel(initialData);
+  };
+
   return (
-    <section id="confirmacion">
+    <section id="confirmacion" className="visible">
       <h2 className="bellefair-regular">Confirmación</h2>
-      <div className="form-container">
-        <p>¡Hola! Estas invitado a nuestra boda. Esperamos puedas asistir.</p>
-        <form className="flex flex-column gap-sm">
-          <label htmlFor="">Tienes 3 boletos</label>
-          <p>Confirmo que iremos</p>
-          <div className="boletos-counter">
-            <button
-              className={numBoletos !== 1 ? "" : "white"}
-              disabled={numBoletos === 1}
-              onClick={(e: any) => {
-                e.preventDefault();
-                setNumBoletos(numBoletos - 1);
-              }}
-            >
-              <FontAwesomeIcon icon={faMinus} />
-            </button>
+      {subiendo ? (
+        <CircleLoader className="loader" size={100} color="#000" />
+      ) : (
+        <div className="form-container">
+          {respuestaEnviada ? (
+            <h4>
+              ¡Respuesta enviada! Gracias, si hay algun cambió en los planes
+              puedes volver al link que te dieron para ajustar tus boletos.
+            </h4>
+          ) : (
+            <>
+              <p>
+                ¡Hola {familia}! Estas invitado a nuestra boda. Esperamos puedas
+                asistir.
+              </p>
+              <form
+                className="flex flex-column gap-sm"
+                onSubmit={(e) => e.preventDefault()}
+              >
+                <label htmlFor="">Tienes {numBoletos} boletos</label>
+                <p>Confirmo que iremos</p>
+                <div className="boletos-counter">
+                  <button
+                    className={boletosConfirmados <= 1 ? "white" : ""}
+                    disabled={boletosConfirmados <= 1}
+                    onClick={(e: any) => {
+                      e.preventDefault();
+                      setBoletosConfirmados(boletosConfirmados - 1);
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faMinus} />
+                  </button>
 
-            <label htmlFor="">{numBoletos}</label>
+                  <label htmlFor="">{boletosConfirmados}</label>
 
-            <button
-              className={numBoletos !== 3 ? "" : "white"}
-              disabled={numBoletos === 3}
-              onClick={(e: any) => {
-                e.preventDefault();
-                setNumBoletos(numBoletos + 1);
-              }}
-            >
-              <FontAwesomeIcon icon={faPlus} />
-            </button>
-          </div>
-          <p>personas</p>
-          <label htmlFor="mensaje">Mensaje para los novios</label>
-          <textarea name="mensaje" rows={7} />
+                  <button
+                    className={boletosConfirmados !== numBoletos ? "" : "white"}
+                    disabled={boletosConfirmados === numBoletos}
+                    onClick={(e: any) => {
+                      e.preventDefault();
+                      setBoletosConfirmados(boletosConfirmados + 1);
+                    }}
+                  >
+                    <FontAwesomeIcon icon={faPlus} />
+                  </button>
+                </div>
+                <p>personas</p>
+                <label htmlFor="mensaje">Mensaje para los novios</label>
+                <textarea
+                  name="mensaje"
+                  rows={7}
+                  onChange={(e) => setMensaje(e.target.value)}
+                />
 
-          <button className="confirm-btn">Confirmar</button>
-          <button className="confirm-btn">No Asistiré</button>
-        </form>
-      </div>
+                <button
+                  className="confirm-btn"
+                  onClick={() => handleSubmit("ASISTIRE")}
+                >
+                  Confirmar
+                </button>
+                <button
+                  className="confirm-btn"
+                  onClick={() => handleSubmit("NO ASISTIRE")}
+                >
+                  No Asistiré
+                </button>
+              </form>
+            </>
+          )}
+        </div>
+      )}
     </section>
   );
 }
